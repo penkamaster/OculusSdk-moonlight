fc90d48f7514ad63c9970bf4044c761d2bf91679
Rafael Fernandez Mena <penkamaster@gmail.com>
2018-07-09 19:21:38 +0200

    block back button

--- VrSamples/Native/CinemaSDK/java/com/oculus/cinemasdk/MainActivity.java
+++ VrSamples/Native/CinemaSDK/java/com/oculus/cinemasdk/MainActivity.java
@@ -504,7 +504,11 @@ public class MainActivity extends VrActivity implements AudioManager.OnAudioFocu
 		}else{
 			Log.e("INPUT dispatchKeyEvent OTRO", "KeyEvent source: " + event.getSource());
 		}
-		return super.dispatchKeyEvent(event);
+		if (event.getKeyCode() != KeyEvent.KEYCODE_BACK ) {
+			return super.dispatchKeyEvent(event);
+		}else{
+			return false;
+		}
 	}
 
 
